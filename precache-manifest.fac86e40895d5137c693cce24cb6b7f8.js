4a9cb1aad2b50ca46719d35c8dbf710352bae18d
a-Ksy <aaksoy17@ku.edu.tr>
2020-09-14 16:29:08 +0300

    Updates

---
+++ precache-manifest.fac86e40895d5137c693cce24cb6b7f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e975edf6142cdf04266f43a3452b3f4",
+    "url": "/portfolio-new/index.html"
+  },
+  {
+    "revision": "0c15a82abee04e90ead2",
+    "url": "/portfolio-new/static/css/main.dd5c0987.chunk.css"
+  },
+  {
+    "revision": "afadded3d5d8f7a11a98",
+    "url": "/portfolio-new/static/js/2.ec5ff7f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-new/static/js/2.ec5ff7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c15a82abee04e90ead2",
+    "url": "/portfolio-new/static/js/main.68a54195.chunk.js"
+  },
+  {
+    "revision": "2c7bf4a8fd79356f7121",
+    "url": "/portfolio-new/static/js/runtime-main.5ab306d6.js"
+  },
+  {
+    "revision": "419d90a85f558be081bb0bf29ebfc040",
+    "url": "/portfolio-new/static/media/AtahanAksoy.419d90a8.pdf"
+  },
+  {
+    "revision": "9d62cb9fcefe1cb94d2d90553cbef3f1",
+    "url": "/portfolio-new/static/media/Proxima Nova Alt Bold.9d62cb9f.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/portfolio-new/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "b1099f31a4525cfda309da8616ca8308",
+    "url": "/portfolio-new/static/media/kugpa.b1099f31.JPG"
+  },
+  {
+    "revision": "2ac5d68ffa170b9a257f20713277bc25",
+    "url": "/portfolio-new/static/media/laborodor.2ac5d68f.JPG"
+  },
+  {
+    "revision": "7570ee794160a7c978cbc2bbc1240f36",
+    "url": "/portfolio-new/static/media/me2.7570ee79.png"
+  }
+]);
