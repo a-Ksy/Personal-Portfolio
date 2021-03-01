3e21f3a96bab9353ff1efa3c1635c6100377b892
a-Ksy <aaksoy17@ku.edu.tr>
2021-03-01 16:14:35 +0300

    Updates

---
+++ precache-manifest.14291fc8a60e65f3071c7062bcedf3fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a21c756d22c2fdf9a65576ccca6595",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "392246cf24d23e1b0c26",
+    "url": "/personal-portfolio/static/css/main.2a45fb8d.chunk.css"
+  },
+  {
+    "revision": "b067c1be7917f1c3b7d1",
+    "url": "/personal-portfolio/static/js/2.7cc8bb0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-portfolio/static/js/2.7cc8bb0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392246cf24d23e1b0c26",
+    "url": "/personal-portfolio/static/js/main.36eeab04.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  },
+  {
+    "revision": "96b28161386b87f5630374a30a6ef248",
+    "url": "/personal-portfolio/static/media/AtahanAksoy.96b28161.pdf"
+  },
+  {
+    "revision": "9d62cb9fcefe1cb94d2d90553cbef3f1",
+    "url": "/personal-portfolio/static/media/Proxima Nova Alt Bold.9d62cb9f.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/personal-portfolio/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "843a4e0ccc1026eba5ee683a1f310a11",
+    "url": "/personal-portfolio/static/media/codenames.843a4e0c.jpg"
+  },
+  {
+    "revision": "b1099f31a4525cfda309da8616ca8308",
+    "url": "/personal-portfolio/static/media/kugpa.b1099f31.JPG"
+  },
+  {
+    "revision": "2ac5d68ffa170b9a257f20713277bc25",
+    "url": "/personal-portfolio/static/media/laborodor.2ac5d68f.JPG"
+  },
+  {
+    "revision": "7570ee794160a7c978cbc2bbc1240f36",
+    "url": "/personal-portfolio/static/media/me2.7570ee79.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3449269b8e7978f103a61a0185a363bc.js"
+  "/personal-portfolio/precache-manifest.14291fc8a60e65f3071c7062bcedf3fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
